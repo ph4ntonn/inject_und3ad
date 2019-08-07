418547bec5ea7705775781bf7d76f6c4cd125561
ph4ntom <45198234+phantom11235@users.noreply.github.com>
2019-08-07 17:45:47 +0800

    Add files via upload

---
+++ language/en.js
@@ -0,0 +1,24 @@
+/**
+ * en_US
+ */
+
+module.exports = {
+    title: "Inject the undead php webshell",
+    toolbar: {
+        new: "New remote control file"
+    },
+    message: {
+        inject_success: "Successfully inject! The address of remote control file has been recorded!"
+    },
+    prompt: {
+        rm_file: "Please enter the remote control file address and timeout's value(spilted by ###)"
+    },
+    cell:{
+        path:"Remote control file history"
+    },
+    error:{
+        wrong:"Injection fail!",
+        value_wrong:"Illegal input ! Please check your input!"
+    }
+
+};
---
+++ language/zh.js
@@ -0,0 +1,24 @@
+/**
+ * zh_CN
+ */
+
+module.exports = {
+    title: "植入不死php webshell",
+    toolbar: {
+        new: "远端文件"
+    },
+    message: {
+        inject_success: "植入成功,远程控制文件已被记录!"
+    },
+    prompt: {
+        rm_file: "请输入远端控制文件地址及轮询时间(以###分隔)"
+    },
+    cell:{
+        path:"历史远端控制文件"
+    },
+    error:{
+        wrong:"植入失败",
+        value_wrong:"输入非法，请输入有效的轮询时间或检查分隔符是否为\'###\'！"
+    }
+
+};
